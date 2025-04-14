f5f301549871c43c566d97b3dc1ee7c7a6b7cf5c
mail <hakkicanbuluc@gmail.com>
2025-04-14 17:39:57 +0300

    [Feat] user_service get()

--- lib/services/api/user_service.dart
+++ lib/services/api/user_service.dart
@@ -8,12 +8,24 @@ class UserService {
 
   UserService(this.dio);
 
-  Future<User?> get({String? email = ""}) async {
-    return User(
-        name: "Test",
-        surname: "Testoğlu",
-        email: "test@test.com",
-        phoneNo: "5551234567890");
+  Future<User?> get() async {
+    try {
+      String url = API.getUrl(Controllers.users, "Get");
+      Response<Map<String, dynamic>> response = await dio.get(url);
+
+      if (response.statusCode == 200) {
+        return User.fromJson(response.data!);
+      }
+
+      throw API.getError(url, response);
+    } on DioException catch (e) {
+      Response response = e.response!;
+      if (response.statusCode == 500) {
+        return null;
+      }
+
+      throw API.getError(response.realUri.toString(), response);
+    }
   }
 
   Future<User> signIn(Map<String, dynamic> userData) async {
