c98d67c35016302c70a4ed6c5e3f65195784b3c0
mail <hakkicanbuluc@gmail.com>
2025-05-20 10:18:24 +0300

    [Feat] UserService.get and signIn()

--- lib/services/apis/my_api/user_service.dart
+++ lib/services/apis/my_api/user_service.dart
@@ -1,6 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:kurbandas/core/domain/entities/user.dart';
-import 'package:kurbandas/services/apis/my_api/my_api.dart';
+
+import 'my_api.dart';
 
 class UserService {
   final Dio dio;
@@ -8,13 +9,39 @@ class UserService {
   UserService(this.dio);
 
   Future<User?> get() async {
-    return Future.value(
-        User(name: "Ahmet", surname: "Balaman", email: "ahmet@test.com"));
+    try {
+      String url = MyAPI.getUrl(Controllers.users, "Get");
+      Response<Map<String, dynamic>> response = await dio.get(url);
+
+      if (response.statusCode == 200) {
+        return User.fromJson(response.data!);
+      }
+
+      throw MyAPI.getError(url, response);
+    } on DioException catch (e) {
+      Response response = e.response!;
+      if (response.statusCode == 500) {
+        return null;
+      }
+
+      throw MyAPI.getError(response.realUri.toString(), response);
+    }
   }
 
   Future<User> signIn(Map<String, dynamic> userData) async {
-    return Future.value(
-        User(name: "Ahmet", surname: "Balaman", email: "ahmet@test.com"));
+    try {
+      String url = MyAPI.getUrl(Controllers.users, "SignIn");
+      Response<Map<String, dynamic>> response =
+          await dio.post<Map<String, dynamic>>(url, data: userData);
+
+      if (response.statusCode == 200) {
+        return User.fromJson(response.data!);
+      }
+
+      throw MyAPI.getError(url, response);
+    } on DioException catch (e) {
+      throw MyAPI.getDioException(e);
+    }
   }
 
   Future<User> update(Map<String, dynamic> data) async {
